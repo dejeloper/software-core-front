c93364f50856ea85c6652c4762753f07f5070618
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2024-01-22 19:01:17 -0500

    Update target to ES6 in tsconfig.json, remove unused code in tailwind.config.ts, and update dependencies in package.json

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,23 @@
 import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
 import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Software CORE',
-  description: 'Software CORE - Software para la gestión de la calidad'
+  description: 'Software CORE - Software para la gestión de la calidad',
+  icons: [{ url: '/note.svg', rel: 'icon', type: 'image/svg+xml' }]
 }
 
 export default function RootLayout ({
   children
-}: {
+}: Readonly<{
   children: React.ReactNode
-}) {
+}>) {
   return (
-    <html lang="es-co">
-      <body>{children}</body>
+    <html lang='es-co'>
+      <body className={inter.className}>{children}</body>
     </html>
   )
 }
